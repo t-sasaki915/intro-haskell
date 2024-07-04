58c6f2b5f3fce60da9c47700463fda073085678d
Sasaki Touma <netst915@gmail.com>
2024-07-04 16:03:19 +0900

    try lucid

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Lib
+import Lucid
 
 main :: IO ()
-main = someFunc
+main = print $ renderBS indexHtml
+
+indexHtml :: Html ()
+indexHtml = do
+    doctype_
+    html_ [lang_ "ja"] $ do
+        head_ $ do
+            title_ [] "TEST"
+        
+        body_ $ do
+            h1_ [] "TESTTEST"
